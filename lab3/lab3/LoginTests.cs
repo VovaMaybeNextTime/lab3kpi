d0c1615f2c5712873a83d82dd6fcb7ab63950292
VovaMaybeNextTime <48494728+VovaMaybeNextTime@users.noreply.github.com>
2021-03-24 22:53:00 +0200

    Add files via upload

---
+++ lab3/lab3/LoginTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Tests
+{
+    [TestFixture]
+    class LoginTests : BaseTest
+    {
+        private static LoginHelper loginHelper = new LoginHelper();
+
+        [Test]
+        public static void CorrectLoginTest()
+        {
+            string email = "pro1001111111@bk.ru";
+            string password = "Vova28042001";
+
+            string expectedUserName = "NaVi_strelok_iz_kustov";
+
+            loginHelper
+                .Login(email, password)
+                .AssertUserName(expectedUserName);
+        }
+
+
+        [Test]
+        public static void InCorrectLoginTest()
+        {
+            string email = "pro1001111111@bk.ru";
+            string password = "rthgthgfh";
+
+            loginHelper
+                .Login(email, password)
+                .AssertIncorrectData();
+        }
+    }
+}
---
+++ lab3/lab3/SearchTests.cs
@@ -0,0 +1,42 @@
+﻿using lab3.TestHelpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Tests
+{
+    [TestFixture]
+    class SearchTests : BaseTest
+    {
+        private static SearchHelper searchHelper = new SearchHelper();
+        [Test]
+        public void ShowTipsTest()
+        {
+            string searchRequest = "asd\n";
+
+            searchHelper.Search(searchRequest)
+                .AssertTipsIsPresent();
+        }
+
+        [Test]
+        public void SearchIsCorrectTest()
+        {
+            string searchRequest = "zxc\n";
+
+            searchHelper.Search(searchRequest)
+                .AssertProductListIsShown();
+        }
+
+        [Test]
+        public void SearchIsNotSuccessfullTest()
+        {
+            string searchRequest = "fghtdtyj\n";
+
+            searchHelper.Search(searchRequest)
+                .AssertMeesage();
+        }
+    }
+}
